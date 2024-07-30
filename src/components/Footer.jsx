86f7f89a5f75dc3cedb9de93a29c5cd3d07d1d42
Sammuel Kodzogan LEKPOR <samuellekpor@gmail.com>
2024-07-30 00:03:16 +0000

    Displayed information on the sidebar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-export default function Footer() {
+export default function Footer(props) {
+  const { showModal, handleToggleModal } = props
   return (
     <footer>
       <div className="bgGradient"></div>
@@ -6,7 +7,7 @@ export default function Footer() {
         <h2>The Brutal Martin Landscape</h2>
         <h1>APOD PROJECT</h1>
       </div>
-      <button>
+      <button onClick={handleToggleModal}>
         <i className="fa-solid fa-circle-info"></i>
       </button>
     </footer>
