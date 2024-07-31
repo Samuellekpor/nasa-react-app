490bfebc2aec78c3dabfdce90aa7ea17a9dc9243
Sammuel Kodzogan LEKPOR <samuellekpor@gmail.com>
2024-07-31 23:33:30 +0000

    Fetched data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
     setShowModal(!showModal);
   }
+
+  useEffect(() => {
+    async function fetchApiData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      try {
+        const res = await fetch(url)
+        const apiData = await res.json()
+        setData(apiData)
+        console.log('Data\n', apiData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    //fetchApiData()
+    
+  }, [])
   
   return (
     <>
-      <Main />
+      {data ? (<Main />) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-spinner"></i>
+        </div>
+      )}
       {showModal && (
         <SideBar handleToggleModal={handleToggleModal}/>
       )}
