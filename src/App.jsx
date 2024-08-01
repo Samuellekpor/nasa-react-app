ce4937a187f792d4eda9f679bdeb7105f7c46f67
unknown <samuel.lekpor@gmail.com>
2024-08-01 11:38:44 +0000

    Cached the data

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,24 @@ function App() {
     async function fetchApiData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+      const today = (new Date()).toDateString();
+      const localKey = `NASA_${today}`;
+      const localData = localStorage.getItem(localKey);
+      if (localData) {
+        const apiData = JSON.parse(localData);
+        setData(apiData);
+        console.log('Fetched from cache today')
+        return;
+      }
+      localStorage.clear()
+
       try {
         const res = await fetch(url)
         const apiData = await res.json()
+        localStorage.setItem(localKey, JSON.stringify(apiData));
         setData(apiData)
-        console.log('Data\n', apiData);
+        console.log('Fetched from API today');
       } catch (error) {
         console.log(error.message);
       }
