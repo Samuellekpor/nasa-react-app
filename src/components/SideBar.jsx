22a2c18f5561a0983b0a325f7322709300a2cf94
unknown <samuel.lekpor@gmail.com>
2024-08-01 00:49:48 +0000

    Displayed the data

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,14 @@
 export default function SideBar(props) {
-  const { handleToggleModal } = props
+  const { handleToggleModal, data } = props
 
   return (
     <div className="sidebar">
       <div onClick={handleToggleModal} className="bgOverlay"></div>
       <div className="sidebarContents">
-        <h2>The Brutal Martin Landscape</h2>
+        <h2>{data?.title}</h2>
         <div>
-          <p>Description</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h3>Description</h3>
+          <p>{data?.explanation}</p>
         </div>
         <button onClick={handleToggleModal}>
           <i className="fa-solid fa-arrow-right"></i>
